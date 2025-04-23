f02f0c59ab253a70257fb4565e3a8ca4db6247cc
Suphakit P. <me@suphakit.net>
2025-04-23 14:33:36 +0700

    chore<page>: fix button coloe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ import ProductCardMini from '../components/ProductCardMini.vue'
           </p>
           <a
             href="/listing"
-            class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Go Shopping
             <svg
